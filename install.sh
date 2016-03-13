a5efaa906855007e0e031674f7d5c0cff0f8ce6e
tenkai9 <tenkai.nine@gmail.com>
2016-03-13 20:17:36 +0300

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-dir = $(pwd)
+dir=$(pwd)
 
-ln -s $dir/.config/openbox/lxde-rc.xml ~/.config/openbox/lxde-rc.xml
-mkdir -p ~/.config/i3/
-ln -s $dir/.config/i3/config ~/.config/i3/config
-ln -s $dir/.fonts ~/.fonts
 ln -s $dir/.myscripts ~/.myscripts
 ln -s $dir/.vim ~/.vim
 ln -s $dir/.zsh-git-prompt ~/.zsh-git-prompt
@@ -13,3 +9,5 @@ ln -s $dir/.bash_profile ~/.bash_profile
 ln -s $dir/.vimrc ~/.vimrc
 ln -s $dir/.zshrc ~/.zshrc
 ln -s $dir/.rbenv ~/.rbenv
+ln -s $dir/.pryrc ~/.pryrc
+ln -s $dir/.aprc ~/.aprc
